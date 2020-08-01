4b09cda7b4007f8aac8d90969c296e9894d4ceb6
Rafał <rafal.nowak@lug.com.pl>
2020-08-01 22:55:01 +0200

    d

--- covid.js
+++ covid.js
@@ -9,62 +9,10 @@
             id: "dataRep",
             alias:"Data",
             dataType: tableau.dataTypeEnum.date
-        }, {
-            id: "day",
-            alias: "Stan",
-            dataType: tableau.dataTypeEnum.int
-        }, {
-           id: "month",
-           alias: "Zarażeni",
-           dataType: tableau.dataTypeEnum.int
-        },
-        {
-            id: "year",
-            alias:"Intensywna terapia",
-            dataType: tableau.dataTypeEnum.int
-        },
-        {
-           id: "cases",
-           alias:"Ogółem hospitalizowani",
-          dataType: tableau.dataTypeEnum.int
-      },
-    {
-          id: "deaths",
-          alias:"Izolacja domowa",
-          dataType: tableau.dataTypeEnum.int
-    },
-    {
-           id: "countriesAndTerritories",
-           alias:"Ogółem zarażeni",
-           dataType: tableau.dataTypeEnum.int
-  },
-  {
-          id: "geoID",
-           alias:"geoID",
-          dataType: tableau.dataTypeEnum.string
-},
-{
-         id: "countryterritoryCode",
-         alias:"Kod kraju",
-          dataType: tableau.dataTypeEnum.string
-},
-{
-          id: "dopData2019",
-          alias:"opData2019i",
-          dataType: tableau.dataTypeEnum.int
-}, {
-          id: "continentExp",
-          alias:"kontynent",
-         dataType: tableau.dataTypeEnum.int
-},
- {
-          id: "Cumulative_number_for_14_days_of_COVID-19_cases_per_100000",
-          alias:"14 dni per 100k",
-          dataType: tableau.dataTypeEnum.int
-}, ];
+        }, ];
         var tableSchema = {
-            id: "opendata",
-            alias: "Dane dotyczące COVID-19",
+            id: "covid19",
+            alias: "covid19",
             columns: cols
         };
 
@@ -81,17 +29,8 @@
             // Iterate over the JSON object
             for (var i = 0; i < data.length; i++) {
                 tableData.push({
-                    "dataRep":data[i]["dataRep"],  // metti in data la response al campo "data"
-                    "day":data[i]["day"],
-                    "month":data[i]["month"],
-                    "years":data[i]["years"],
-                    "cases":data[i]["cases"],
-                    "countriesAndTerritories":data[i]["countriesAndTerritories"],
-                    "geoID":data[i]["geoID"],
-                    "countryterritoryCode":data[i]["ountryterritoryCode"],
-                    "popData2019":data[i]["popData2019"],
-                    "continentExp":data[i]["ontinentExp"],
-                    "Cumulative_number_for_14_days_of_COVID-19_cases_per_100000":data[i]["dCumulative_number_for_14_days_of_COVID-19_cases_per_100000"],
+                    "dataRep":data[i]["dataRep"],  
+             
                     
                 });
             }
@@ -106,7 +45,7 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "OpenPuglia"; // This will be the data source name in Tableau
+            tableau.connectionName = "covid19"; // This will be the data source name in Tableau
             tableau.submit();                     // This sends the connector object to Tableau
         });
     });
